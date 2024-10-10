e001d01907f584866bba9e41d17634d59c24159f
kenny-chua <kenny.chua.edu@outlook.com>
2024-10-10 18:37:07 -0400

    Commit before Robin 03

---
+++ text.py
@@ -0,0 +1,138 @@
+useful_links = """
+    - [PDM Onboarding](https://pybites.circle.so/c/pybites-developer-mindset-pdm-program/)
+    - [Circle Community](https://pybites.circle.so/feed)
+    - [Pybites Podcast](https://www.pybitespodcast.com/)
+    - [Pybites YouTube Channel](https://www.youtube.com/@Pybites)
+    - [Articles/Blog Posts](https://pybit.es/articles/)
+    - [Code Clinics](https://pybites.circle.so/c/circle-coaching-calls/?topics=35656)
+    - [Mindset Clinics](https://pybites.circle.so/c/circle-coaching-calls/?topics=35655)
+    - [Code Challenge](https://codechalleng.es/)
+    """
+
+gh_pdm_links = """
+    - [PDM Main Repository](https://github.com/pybites/PDM_KennyC)
+    - [PDM Project](https://github.com/users/pybites/projects/38)
+    - [PDM Issues](https://github.com/pybites/PDM_KennyC/issues)
+    - [PDM Milestones](https://github.com/pybites/PDM_KennyC/milestones)
+    """
+
+gh_app_links = """
+    - [Application Repository](https://github.com/kenny-chua/pdm-companion)
+    - [Application Issues](https://github.com/kenny-chua/pdm-companion/issues)
+    - [Application Project](https://github.com/users/kenny-chua/projects/4)
+    """
+
+T1 = """
+I had not coded at all but I wanted to so, I went to university and got a bachelor’s in computer science, then a master’s in information technology. I had a difficult time finding a job because I lacked experience coding. I struggled answering the questions at technical reviews and solving the coding challenges. I had the knowledge I gained in school but was reluctant to deep dive on my own.
+
+Under Bob and Julian’s guidance in the Pybites Developer Mindset program, I was able to develop a couple of apps to Minimum Viable Product with one of them online @Render and got another one close to MVP. They also encourage starting a blog, which I am about to post my first article, and putting yourself out there on social media. I am still looking for a job but with the confidence and skill I gained through the program, I am sure it won’t take me long.
+
+--Jarrod N (USA)
+PDM Program Client
+"""
+
+T2 = """
+I am a network engineer, so coding has never been on my “radar.” I figured if I could automate some of the mundanity it would be less to worry about from day to day.
+
+I discovered Pybites when searching the internet to understand python classes, I found the code challenge platform shortly after and dove in. As a beginner I can say that coding (in any language) has always seemed daunting so I made no effort to learn. The Pybites platform is structured so that you are coding from the first challenge and I quickly began to see how beneficial the Pybites platform could be, to say nothing of Python itself. I took the plunge and joined the Pybites Developer Mindset program (PDM) to further my growth. Fast forward 11 months and I now have 2 apps of my own creation. My confidence has increased exponentially and I have real world experience in testing, software best practices, deployment, performance, and design patterns.
+
+For anyone wanting to learn Python I can’t recommend Pybites enough. Bob, Julian, & the entire community have all contributed to my development. You will be surprised at what Pybites can help you accomplish.
+
+Thanks Bob, Julian, and all the other Pybites team members!!
+
+--Dabrien S (USA)
+PDM Program Client
+"""
+
+T3 = """
+Python rekindled my love of programming and I was very keen to increase my skills and experience.
+
+I decided that two things would really help me to move forward: collaborating on a real project with a mentor, and becoming part of a supportive and experienced community of Python developers.
+
+The PyBites PDM Program gave me both of those things and so much more. A wonderful and unexpected bonus was the motivation, support and camaraderie gained by sharing the journey with other clients in the program. I learned many new skills (technical and mindset), built some cool apps, made some great friends and genuinely had a blast.
+
+If you have the opportunity to take the program – just do it!
+
+--Craig R (UK)
+PDM Program Client
+"""
+
+T4 = """
+PDM helped me start transforming my knowledge of Python from an “occasionally and crudely used tool” into my main tool of choice.
+
+The difference between learning by yourself and taking PDM is like the difference between navigating through an unknown town without a map vs. having a map. Both options can reach the goal, but with a map it’s way faster and a more efficient use of your time.
+
+And the same applies to PDM: thanks to good mentoring and proper code reviews from an experienced developer, I was able to keep my focus on the important things. Shortly after PDM I even changed to a Python focused job, which is something I could not do with my previous knowledge of Python.
+
+--Josef P (Czech Republic)
+PDM Program Client
+"""
+
+T5 = """
+The time with my tutor Ryan was invaluable to me. Direct access to a subject expert in Django on a 1 to 1 basis makes Pybites stand out as different.
+
+Over a few years I had tried to revive my hobby project without success. Ryan and Pybites have helped get me back on the road and equipped me with a much greater understanding of modern Django.
+
+--David M (UK)
+PDM Program Client
+"""
+
+T6 = """
+I’m astonished at how far I’ve come in my career after PDM.
+
+I went from writing my very first Pull Request in the program to leading (by example) several seasoned engineers in the field of Data Engineering.
+
+I now give demos during office hours every week where I provide “bites” of helpful refactoring ideas and showing how we can leverage more of the Standard Library.
+
+This is all in no small part due to the lessons I learned through Pybites and the PDM program. What may seem like just little nuggets of info become an invaluable tool set.
+
+--Ryan B (USA)
+PDM Program Client
+"""
+
+thematic_code = '''
+    def pybites_developer_program(you):
+        journey = ["foundation", "code challenge", "build_app", "open_source"]
+        growth_mindset = "julian sequeira"
+        
+        for step in journey:
+            you.take_step(step)
+        
+        if you.mindset == f'listen to {growth_mindset} and you.reflect("progress"):
+            return "SUCCESS!! 🎉👨‍💻👩‍💻"
+
+        return "Keep Growing... 🌱"
+    '''
+
+html_code_block_style = """
+    <div style="text-align: center; margin-top: 50px;">
+        <div style="
+            display: inline-block;
+            padding: 20px;
+            background-color: transparent;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            transition: transform 0.5s ease-in-out;">
+            <p style="font-size: 1.5em; color: white; font-family: 'IBM Plex Sans', sans-serif; font-weight: bold; animation: blinker 5s linear infinite;">Congragulations!! 🎉 You are  PCPD-P CERTIFIED.</p>
+        </div>
+    </div>
+    <style>
+    @keyframes blinker {
+        100% { opacity: 0; }
+    }
+    </style>
+    """
+
+testimonial_style = """
+    <div style="
+        padding: 20px;
+        margin-top: 20px;
+        border-radius: 10px;
+        background-color: #f0f0f0;
+        color: #333;
+        box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+        text-align: center;
+    ">
+        <p style="font-size: 1.2em; font-style: italic;">{}</p>
+    </div>
+    """
